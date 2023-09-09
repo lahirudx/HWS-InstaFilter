710eba5eea3109269c2e3e5cdfcc9ab4f7bf8ae7
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-09-09 17:40:33 +0530

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Lahiru Amarasooriya on 2023-09-09.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
